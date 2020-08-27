cedb2e44e9594699ae179f0b8b493a5920d56643
Alberto Herrera Vargas <albertoherreravargas@gmail.com>
2020-08-27 10:08:48 +0200

    Updating main logo

--- src/components/InformativeMainPaneComponent.js
+++ src/components/InformativeMainPaneComponent.js
@@ -12,7 +12,7 @@ function InformativeMainPaneComponent (props) {
             </div>
             <div className="informative-main-pane-message">
                 <p>Esta aplicación web está destinada a la visualización de una manera sencilla de la información que podemos obtener de la medición de ondas a través de la electromiografía.</p>
-                <p>Gracias a esta página podrás entender qué está sucediendo en tus células neuromuscualres mientras realizas ejercicio. Si aún no tienes datos, ¡expórtalos desde la pestaña de <span className="bold">Datos</span>!</p>
+                <p>Gracias a esta página podrás entender qué está sucediendo en tus células neuromusculares mientras realizas ejercicio. Si aún no tienes datos, ¡puedes usar nuestros <span className="bold">datos de prueba</span>!</p>
             </div>
         </div>
     )
