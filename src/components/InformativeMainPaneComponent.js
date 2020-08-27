8c5deaf2d3fc09fd0bbfe39495e1e4e59596b5cd
Alberto Herrera Vargas <albertoherreravargas@gmail.com>
2020-08-27 10:18:33 +0200

    Fixing wording and button position

--- src/components/InformativeMainPaneComponent.js
+++ src/components/InformativeMainPaneComponent.js
@@ -11,7 +11,7 @@ function InformativeMainPaneComponent (props) {
             <p>Bienvenido {userEntity.username}</p>
             </div>
             <div className="informative-main-pane-message">
-                <p>Esta aplicación web está destinada a la visualización de una manera sencilla de la información que podemos obtener de la medición de ondas a través de la electromiografía.</p>
+                <p>Esta aplicación web está destinada a la visualización de una manera sencilla de la información que podemos obtener de la medición de señales a través de la electromiografía.</p>
                 <p>Gracias a esta página podrás entender qué está sucediendo en tus células neuromusculares mientras realizas ejercicio. Si aún no tienes datos, ¡puedes usar nuestros <span className="bold">datos de prueba</span>!</p>
             </div>
         </div>
